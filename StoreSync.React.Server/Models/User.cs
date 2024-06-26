6e316c22b3ded2c152ed3cb33bf033674597f332
manfortun <m.fortun@arcanys.com>
2024-06-26 16:37:34 +0800

    Add token service

---
+++ StoreSync.React.Server/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreSync.React.Server.Models;
+
+public class User
+{
+    [Key]
+    public string Id { get; set; }
+    public virtual IdentityUser IdentityUser { get; set; } = default!;
+
+    [Required]
+    [MaxLength(255)]
+    public string FirstName { get; set; } = default!;
+
+    [Required]
+    [MaxLength(255)]
+    public string LastName { get; set; } = default!;
+}
---
+++ StoreSync.React.Server/Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StoreSync.React.Server.Services;
+
+public class TokenService
+{
+    private readonly IConfiguration _config;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string GenerateJwtToken(IEnumerable<Claim> claims)
+    {
+        string jwtKey = _config["Jwt:SecretKey"] ?? throw new InvalidOperationException("No secret key in application settings.");
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        return GenerateJwtToken(credentials, claims);
+    }
+
+    public string GenerateJwtToken(SigningCredentials credentials, IEnumerable<Claim> claims)
+    {
+        ArgumentNullException.ThrowIfNull(credentials);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"] ?? throw new InvalidOperationException("No issuer in application settings."),
+            audience: _config["Jwt:Audience"] ?? throw new InvalidOperationException("No audience in application settings."),
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(1),
+            signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
